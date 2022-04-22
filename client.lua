faaf4c32d16e032dfca6e161e26750accc747333
GouveiaXS <99211701+GouveiaXS@users.noreply.github.com>
2022-04-22 09:06:26 -0600

    QBcore handling update

--- client.lua
+++ client.lua
@@ -30,11 +30,19 @@ CreateThread(function()
 	elseif Config.UseQBCore then
 
 		QBCore = exports["qb-core"]:GetCoreObject()
-		Wait(200)
-		PlayerData = QBCore.Functions.GetPlayerData()
 
-			PlayerJob = PlayerData.job.name
-			PlayerGrade = PlayerData.job.grade.level
+		PlayerData = QBCore.Functions.GetPlayerData()
+			
+		CreateThread(function ()
+			while true do
+				if PlayerData.citizenid ~= nil then
+					PlayerJob = PlayerData.job.name
+					PlayerGrade = PlayerData.job.grade.level
+					break
+				end
+				Wait(100)
+			end
+		end)
 
 
 		RegisterNetEvent("QBCore:Client:OnJobUpdate", function(job)
