be497bf8df0ed5cc2cd1304c16f01b09dbb96187
AngelicXS <99211701+GouveiaXS@users.noreply.github.com>
2023-01-31 23:51:05 -0700

    ESX = exports["es_extended"]:getSharedObject()

--- client.lua
+++ client.lua
@@ -9,10 +9,7 @@ local DetectorEntity
 
 CreateThread(function()
 	if Config.UseESX then
-		while ESX == nil do
-			TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
-			Wait(0)
-		end
+		ESX = exports["es_extended"]:getSharedObject()
 	
 		while not ESX.IsPlayerLoaded() do
 			Wait(100)
