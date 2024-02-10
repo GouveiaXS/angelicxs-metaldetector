7ffd06e034c515cf538680e0cc5c26c0c21dc216
®️BYLICKILABS™️ <109308073+bylickilabs@users.noreply.github.com>
2024-02-10 11:34:36 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,12 +1,4 @@
-ESX = nil
-QBcore = nil
-
-if Config.UseESX then
-    ESX = exports["es_extended"]:getSharedObject()
-elseif Config.UseQBCore then
-    QBCore = exports['qb-core']:GetCoreObject()
-end
--- Global Syncs
+ESX = exports["es_extended"]:getSharedObject()
 
 RegisterServerEvent('angelicxs-MDetector:Server:SoundPlayer')
 AddEventHandler('angelicxs-MDetector:Server:SoundPlayer', function(args)
