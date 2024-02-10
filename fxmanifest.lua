6703396321baf13771f2a812abd22b3987a104ae
®️Bylickilabs™️ <102898784+G3N3RATION-X@users.noreply.github.com>
2024-02-10 11:43:57 +0100

    plastic surgery
    Updated to FIVEM, and optimized
    
    Co-Authored-By: ®️BYLICKILABS™️ <109308073+bylickilabs@users.noreply.github.com>
    Co-Authored-By: G3N3RATION-X <102898784+g3n3ration-x@users.noreply.github.com>
    Co-Authored-By: AngelicXS <99211701+GouveiaXS@users.noreply.github.com>

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,10 +4,10 @@ game 'gta5'
 author 'AngelicXS'
 version '1.1'
 
-data_file 'DLC_ITYP_REQUEST' 'stream/props/*'
-
 client_script 'client.lua'
 
 server_script 'server.lua'
 
 shared_script 'config.lua'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ch_prop_ch_casino_security.ytyp'
