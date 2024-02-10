7788b0c1164c3ee6849a3bbbdf26ad5468f7cc48
®️BYLICKILABS™️ <109308073+bylickilabs@users.noreply.github.com>
2024-02-10 11:56:28 +0100

    Update fxmanifest.lua
    es_extended tables imported into the resource

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,10 +4,10 @@ game 'gta5'
 author 'AngelicXS'
 version '1.1'
 
+shared_script '@es_extended/imports.lua'
+
 client_script 'client.lua'
-
 server_script 'server.lua'
-
 shared_script 'config.lua'
 
 data_file 'DLC_ITYP_REQUEST' 'stream/ch_prop_ch_casino_security.ytyp'
